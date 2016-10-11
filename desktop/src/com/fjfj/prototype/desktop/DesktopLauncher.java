250016ec74a1d48c5a4feb81f0960f6bb500f4dc
opot <youtolife@yandex.ru>
2016-10-11 17:36:54 +0300

    Library init commit.

---
+++ desktop/src/com/fjfj/prototype/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.fjfj.prototype.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.fjfj.prototype.MainGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MainGame(), config);
+	}
+}
